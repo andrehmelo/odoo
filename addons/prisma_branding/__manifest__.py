d68ce6fdeb3e5b16e7a85bab0d9b562415651866
Pedro Silveira <pedroruanosilveira@tecnico.ulisboa.pt>
2025-12-22 19:26:17 +0000

    feat: start prisma navbar rework

---
+++ addons/prisma_branding/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Prisma Branding",
+    "summary": "Custom header sidebar branding for Prisma Fleet",
+    "version": "1.0",
+    "depends": ["web"],
+    "data": [],
+    "assets": {
+        "web.assets_backend": [
+            "prisma_branding/static/src/scss/branding.scss",
+        ],
+    },
+}
